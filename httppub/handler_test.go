5faea35211282c8d361347c93c735ecaa519ab36
embr <git@liclac.eu>
2020-06-01 21:43:08 +0100

    Test for simple indexes

--- httppub/handler_test.go
+++ httppub/handler_test.go
@@ -56,3 +56,46 @@ func TestHandlerNoIndex(t *testing.T) {
 		assert.Equal(t, "ref: refs/heads/master", string(body))
 	})
 }
+
+func TestHandlerSimpleIndex(t *testing.T) {
+	baseFS := memfs.New()
+	require.NoError(t, baseFS.MkdirAll("/.git", 0000))
+	require.NoError(t, util.WriteFile(baseFS, "/.git/HEAD", []byte("ref: refs/heads/master"), 0000))
+	fs := pubd.FileSystem(baseFS)
+
+	srv := httptest.NewServer(Handler(fs, SimpleIndex(), nil))
+	defer srv.Close()
+
+	t.Run("POST /", func(t *testing.T) {
+		rsp, err := http.Post(srv.URL, "", nil)
+		require.NoError(t, err)
+		defer rsp.Body.Close()
+		assert.Equal(t, http.StatusMethodNotAllowed, rsp.StatusCode)
+		assert.Equal(t, "text/plain; charset=utf-8", rsp.Header.Get("Content-Type"))
+		body, err := ioutil.ReadAll(rsp.Body)
+		require.NoError(t, err)
+		assert.Equal(t, "405 Method Not Allowed\n", string(body))
+	})
+
+	t.Run("GET /", func(t *testing.T) {
+		rsp, err := http.Get(srv.URL)
+		require.NoError(t, err)
+		defer rsp.Body.Close()
+		assert.Equal(t, http.StatusOK, rsp.StatusCode)
+		assert.Equal(t, "text/html; charset=utf-8", rsp.Header.Get("Content-Type"))
+		body, err := ioutil.ReadAll(rsp.Body)
+		require.NoError(t, err)
+		assert.Equal(t, "<pre>\n<a href=\".git/\">.git/</a>\n</pre>\n", string(body))
+	})
+
+	t.Run("GET /.git/HEAD", func(t *testing.T) {
+		rsp, err := http.Get(srv.URL + "/.git/HEAD")
+		require.NoError(t, err)
+		defer rsp.Body.Close()
+		assert.Equal(t, http.StatusOK, rsp.StatusCode)
+		assert.Equal(t, "text/plain; charset=utf-8", rsp.Header.Get("Content-Type"))
+		body, err := ioutil.ReadAll(rsp.Body)
+		require.NoError(t, err)
+		assert.Equal(t, "ref: refs/heads/master", string(body))
+	})
+}
