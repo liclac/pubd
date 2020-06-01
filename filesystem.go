97cde9b0be55e1837efc8203177b71bcc9a9e32f
embr <git@liclac.eu>
2020-06-01 20:27:06 +0100

    It's invalid to Open() a directory with billy

--- filesystem.go
+++ filesystem.go
@@ -46,24 +46,48 @@ func FileSystem(fs billy.Filesystem) http.FileSystem {
 }
 
 func (fs httpFileSystem) Open(name string) (http.File, error) {
+	// Note: It's incorrect to Open() a directory with billy, and it fails with memfs.
+	// Instead, we Stat() the path and return either a httpDir or a httpFile.
+	info, err := fs.Filesystem.Stat(name)
+	if err != nil {
+		return nil, err
+	}
+	if info.IsDir() {
+		return httpDir{name, info, fs.Filesystem}, nil
+	}
 	f, err := fs.Filesystem.Open(name)
-	return httpFile{f, fs.Filesystem, name}, err
+	return httpFile{f, name, info, fs.Filesystem}, err
+}
+
+// Wraps a billy.Filesystem and a directory path in a http.File.
+type httpDir struct {
+	name string
+	info os.FileInfo
+	fs   billy.Filesystem
+}
+
+func (d httpDir) Stat() (os.FileInfo, error) { return d.info, nil }
+func (d httpDir) Close() error               { return nil }
+
+// None of these are valid operations on a directory.
+func (d httpDir) Seek(int64, int) (int64, error) { return 0, os.ErrInvalid }
+func (d httpDir) Read([]byte) (int, error)       { return 0, os.ErrInvalid }
+
+// Note: os.File's Readdir can paginate, but Billy's just returns everything in one go.
+// If this causes issues, adding pagination support is trivial; f.readdir = f.readdir[:num] etc.
+func (d httpDir) Readdir(num int) ([]os.FileInfo, error) {
+	return d.fs.ReadDir(d.name)
 }
 
 // Wraps a billy.File in a http.File.
 type httpFile struct {
 	billy.File
-	fs   billy.Filesystem
 	name string
+	info os.FileInfo
+	fs   billy.Filesystem
 }
 
-// Note: os.File's Readdir can paginate, but Billy's just returns everything in one go.
-// If this causes issues, adding pagination support is trivial; f.readdir = f.readdir[:num] etc.
-func (f httpFile) Readdir(num int) ([]os.FileInfo, error) {
-	return f.fs.ReadDir(f.name)
-}
+func (f httpFile) Stat() (os.FileInfo, error) { return f.info, nil }
 
-// This is on os.File, but on billy.Filesystem.
-func (f httpFile) Stat() (os.FileInfo, error) {
-	return f.fs.Stat(f.name)
-}
+// Can't Readdir something that isn't a directory.
+func (f httpFile) Readdir(num int) ([]os.FileInfo, error) { return nil, os.ErrInvalid }
--- filesystem_test.go
+++ filesystem_test.go
@@ -46,6 +46,71 @@ func TestSortFileInfos(t *testing.T) {
 	}, infos)
 }
 
+func TestFileSystem(t *testing.T) {
+	baseFS := memfs.New()
+	require.NoError(t, baseFS.MkdirAll("/.git", 0000))
+	require.NoError(t, util.WriteFile(baseFS, "/.git/HEAD", []byte("ref: refs/heads/master"), 0000))
+	fs := FileSystem(baseFS)
+
+	t.Run(".git", func(t *testing.T) {
+		d, err := fs.Open("/.git")
+		require.NoError(t, err)
+		assert.IsType(t, httpDir{}, d)
+
+		t.Run("Stat", func(t *testing.T) {
+			info, err := d.Stat()
+			require.NoError(t, err)
+			assert.Equal(t, ".git", info.Name())
+			assert.True(t, info.IsDir())
+		})
+
+		t.Run("Readdir", func(t *testing.T) {
+			infos, err := d.Readdir(-1)
+			require.NoError(t, err)
+			if assert.Len(t, infos, 1) {
+				assert.Equal(t, "HEAD", infos[0].Name())
+				assert.Equal(t, int64(22), infos[0].Size())
+			}
+		})
+
+		t.Run("Read", func(t *testing.T) {
+			_, err := ioutil.ReadAll(d)
+			assert.EqualError(t, err, "invalid argument")
+		})
+
+		t.Run("Close", func(t *testing.T) {
+			assert.NoError(t, d.Close())
+		})
+	})
+
+	t.Run("HEAD", func(t *testing.T) {
+		f, err := fs.Open("/.git/HEAD")
+		require.NoError(t, err)
+		assert.IsType(t, httpFile{}, f)
+
+		t.Run("Stat", func(t *testing.T) {
+			info, err := f.Stat()
+			require.NoError(t, err)
+			assert.Equal(t, "HEAD", info.Name())
+			assert.Equal(t, int64(22), info.Size())
+			assert.False(t, info.IsDir())
+		})
+
+		t.Run("Readdir", func(t *testing.T) {
+			_, err := f.Readdir(-1)
+			assert.EqualError(t, err, "invalid argument")
+		})
+
+		t.Run("Read", func(t *testing.T) {
+			data, err := ioutil.ReadAll(f)
+			assert.NoError(t, err)
+			assert.Equal(t, "ref: refs/heads/master", string(data))
+		})
+
+		assert.NoError(t, f.Close())
+	})
+}
+
 func TestFileSystemExclude(t *testing.T) {
 	testdata := map[string]map[string]bool{
 		"index.html": {
