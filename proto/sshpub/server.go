5c1ea911473ef681d089b792d0efed72cf3e7108
embr <git@liclac.eu>
2020-06-13 02:00:31 +0100

    sshpub: Handle session channels, gracefully decline shell/exec

--- proto/sshpub/server.go
+++ proto/sshpub/server.go
@@ -98,6 +98,9 @@ func (s *Server) ServeConn(ctx context.Context, nConn net.Conn, cfg ssh.ServerCo
 	connL := L.Named("conn")
 	connL.Info("Connected")
 	defer connL.Info("Disconnected")
+
+	var g sync.WaitGroup
+	defer g.Wait()
 	for {
 		select {
 		case newChan, ok := <-newChanC:
@@ -105,9 +108,30 @@ func (s *Server) ServeConn(ctx context.Context, nConn net.Conn, cfg ssh.ServerCo
 				return // Connection closed
 			}
 			typ := newChan.ChannelType()
-			L.Warn("Unknown channel requested", zap.String("type", typ))
-			if err := newChan.Reject(ssh.UnknownChannelType, typ); err != nil {
-				L.Warn("Error rejecting channel", zap.String("type", typ), zap.Error(err))
+			switch typ {
+			case "session":
+				ch, reqC, err := newChan.Accept()
+				if err != nil {
+					L.Warn("Couldn't accept chanel", zap.String("type", typ))
+					break
+				}
+				sessL := L.Named("sess")
+				sessL.Debug("Session started")
+
+				g.Add(1)
+				go func() {
+					defer g.Done()
+					s.ServeSession(ctx, sessL, ch, reqC)
+					if err := ch.Close(); err != nil {
+						sessL.Debug("Error closing session", zap.Error(err))
+					}
+					sessL.Debug("Session closed")
+				}()
+			default:
+				L.Warn("Unknown channel", zap.String("type", typ))
+				if err := newChan.Reject(ssh.UnknownChannelType, typ); err != nil {
+					L.Warn("Error rejecting channel", zap.String("type", typ), zap.Error(err))
+				}
 			}
 		case req, ok := <-reqC:
 			if !ok {
@@ -117,6 +141,46 @@ func (s *Server) ServeConn(ctx context.Context, nConn net.Conn, cfg ssh.ServerCo
 		case <-ctx.Done():
 			L.Debug("Context expired", zap.Error(ctx.Err()))
 			return
+		}
+	}
+}
+
+func (s *Server) ServeSession(ctx context.Context, L *zap.Logger, ch ssh.Channel, reqC <-chan *ssh.Request) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case req, ok := <-reqC:
+			if !ok {
+				return // Connection closed
+			}
+			switch req.Type {
+			// case "subsystem":
+			// ... handle subsystems ...
+
+			// If we wanted to offer a shell, this would be the place to do it.
+			// For now, just print a nicer error message and return exit code 255.
+			case "shell", "exec":
+				if req.WantReply {
+					if err := req.Reply(true, nil); err != nil {
+						L.Error("Error accepting command", zap.String("type", req.Type), zap.Error(err))
+						return
+					}
+				}
+				fmt.Fprintln(ch, "// Shell access is not allowed.")
+				ch.SendRequest("exit-status", false, []byte{0x00, 0x00, 0x00, 0xFF})
+				return
+
+			// Supporting commands that don't do anything, but shouldn't error.
+			case "pty-req", "x11-req", "env", "window-change", "xon-xoff", "signal":
+				L.Debug("Discarding request", zap.String("type", req.Type))
+				lazyReply(L, req, true)
+
+			// ???
+			default:
+				L.Warn("Unknown request", zap.String("type", req.Type))
+				lazyReply(L, req, false)
+			}
 		}
 	}
 }
--- proto/sshpub/util.go
+++ proto/sshpub/util.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 
 	"github.com/go-git/go-billy/v5"
+	"go.uber.org/zap"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -18,4 +19,18 @@ func LoadPrivateKey(fs billy.Filesystem, path string) (ssh.Signer, error) {
 		return nil, err
 	}
 	return ssh.ParsePrivateKey(data)
+}
+
+// If the request wants a reply, decline it, else discard it. Log a warning if declining failed.
+func lazyReply(L *zap.Logger, req *ssh.Request, ok bool) {
+	if !req.WantReply {
+		return
+	}
+	if err := req.Reply(ok, nil); err != nil {
+		msg := "Error declining request"
+		if ok {
+			msg = "Error discarding request"
+		}
+		L.Warn(msg, zap.String("type", req.Type), zap.Error(err))
+	}
 }
